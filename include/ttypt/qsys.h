f92bb8bf2984208903c84cbecdad2e054b638a55
Paulo Andre Azevedo Quirino <quirinpa@gmail.com>
2025-10-15 18:51:41 +0100

    Don't include syslog.h

--- include/ttypt/qsys.h
+++ include/ttypt/qsys.h
@@ -1,14 +1,17 @@
 #ifndef QSYS_H
 #define QSYS_H
 
-#include <syslog.h>
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 #include <stddef.h>
 
 #define UNUSED __attribute__((unused))
 
 #define LOG(TYPE, ...) { \
-	qsyslog(LOG_##TYPE, "%s: ", __func__); \
-	qsyslog(LOG_##TYPE, __VA_ARGS__); \
+	qsyslog(QLOG_##TYPE, "%s: ", __func__); \
+	qsyslog(QLOG_##TYPE, __VA_ARGS__); \
 }
 
 #define WARN(...) LOG(WARNING, __VA_ARGS__)
@@ -20,8 +23,25 @@
 		abort(); \
 	}
 
+enum {
+    QLOG_ERR,
+    QLOG_WARNING,
+    QLOG_NOTICE,
+    QLOG_INFO,
+    QLOG_DEBUG
+};
+
 typedef void (*qsyslog_t)(int type, const char *fmt, ...);
 void qsyslog_set(qsyslog_t logger);
 extern qsyslog_t qsyslog;
+
+/* Cross-platform syslog-like interface */
+void qsys_openlog(const char *ident);
+void qsys_syslog(int priority, const char *fmt, ...);
+void qsys_closelog(void);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif

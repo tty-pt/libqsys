4146e0f95599199ef7785b73e96b6ec6aef62f8b
Paulo Andre Azevedo Quirino <quirinpa@gmail.com>
2025-10-15 18:51:41 +0100

    Don't include syslog.h

--- include/qsys.h
+++ include/qsys.h
@@ -1,14 +1,13 @@
 #ifndef QSYS_H
 #define QSYS_H
 
-#include <syslog.h>
 #include <stddef.h>
 
 #define UNUSED __attribute__((unused))
 
 #define LOG(TYPE, ...) { \
-	qsyslog(LOG_##TYPE, "%s: ", __func__); \
-	qsyslog(LOG_##TYPE, __VA_ARGS__); \
+	qsyslog(QLOG_##TYPE, "%s: ", __func__); \
+	qsyslog(QLOG_##TYPE, __VA_ARGS__); \
 }
 
 #define WARN(...) LOG(WARNING, __VA_ARGS__)
@@ -19,6 +18,14 @@
 		ERR(__VA_ARGS__); \
 		abort(); \
 	}
+
+enum {
+    QLOG_ERR,
+    QLOG_WARNING,
+    QLOG_NOTICE,
+    QLOG_INFO,
+    QLOG_DEBUG
+};
 
 typedef void (*qsyslog_t)(int type, const char *fmt, ...);
 void qsyslog_set(qsyslog_t logger);
